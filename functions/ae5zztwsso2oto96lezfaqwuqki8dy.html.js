24de3f7910d7c222aec726f4c8b5949a0186f094
Joel Maundu <joelmaundukak@gmail.com>
2026-06-05 19:54:21 +0100

    Add new Meta verification file

---
+++ functions/ae5zztwsso2oto96lezfaqwuqki8dy.html.js
@@ -0,0 +1,8 @@
+export function onRequest() {
+  return new Response("ae5zztwsso2oto96lezfaqwuqki8dy", {
+    headers: {
+      "content-type": "text/html; charset=UTF-8",
+      "cache-control": "public, max-age=0, must-revalidate",
+    },
+  });
+}
