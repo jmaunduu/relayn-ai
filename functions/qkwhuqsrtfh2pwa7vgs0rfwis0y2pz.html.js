656fed1b9f374e7d362e89cd890b0eabf5f13405
Joel Maundu <joelmaundukak@gmail.com>
2026-06-05 19:46:39 +0100

    Serve Meta verification file via Pages Function

---
+++ functions/qkwhuqsrtfh2pwa7vgs0rfwis0y2pz.html.js
@@ -0,0 +1,8 @@
+export function onRequest() {
+  return new Response("qkwhuqsrtfh2pwa7vgs0rfwis0y2pz", {
+    headers: {
+      "content-type": "text/html; charset=UTF-8",
+      "cache-control": "public, max-age=0, must-revalidate",
+    },
+  });
+}
